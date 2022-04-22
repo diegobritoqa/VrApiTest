69eb098dc7b47850a2ced9bf3e12d493d3ef3d06
diegobritoqa <83356421+diegobritoqa@users.noreply.github.com>
2022-04-22 13:08:22 -0300

    Update spec_helper.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,8 +4,6 @@ require 'httparty/response/headers'
 require_relative 'helper'
 
 RSpec.configure do |config|
-  # modulo está como global
-  include Contato
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
