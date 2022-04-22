b37412b37c7fc1a5b84a796261cc0ad6db6f0992
diegobritoqa <83356421+diegobritoqa@users.noreply.github.com>
2022-04-22 13:59:11 -0300

    Delete get_spec.rb

--- spec/get_spec.rb
+++
@@ -1,8 +0,0 @@
-describe 'fazer uma requisicao' do
-    it 'get' do
-        
-        @lista = HTTParty.get('https://portal.vr.com.br/api-web/comum/enumerations/VRPAT')
-        puts @lista
-        
-    end
-end
