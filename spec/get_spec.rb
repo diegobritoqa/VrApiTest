5e22833fd1db1ecf22a76d3fc9156a541099f27d
diegobritoqa <83356421+diegobritoqa@users.noreply.github.com>
2022-04-22 13:07:46 -0300

    Update get_spec.rb

--- spec/get_spec.rb
+++ spec/get_spec.rb
@@ -1,9 +1,8 @@
 describe 'fazer uma requisicao' do
     it 'get' do
-        #se a gente não usasse o modulo se chamaria o httparty com o get assim 
+        
         @lista = HTTParty.get('https://portal.vr.com.br/api-web/comum/enumerations/VRPAT')
         puts @lista
-        # @lista = Contato.get('/api-web/comum/enumerations/VRPAT')
-        # puts @lista 
+        
     end
 end
